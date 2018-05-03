fd0e9911143981e2a338d4287732bbb360892421
tara530991 <tara530991@gmail.com>
2018-05-04 04:38:49 +0800

    cart0503-2

---
+++ sql0503-2/tkbshop_product.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: tkbshop
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(20) DEFAULT NULL,
+  `price` int(10) DEFAULT NULL,
+  `stock` int(10) DEFAULT NULL,
+  `uptime` date DEFAULT NULL,
+  `downtime` date DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `pic` varchar(45) DEFAULT NULL,
+  `addtime` date DEFAULT NULL,
+  `changetime` date DEFAULT NULL,
+  `ident` varchar(10) DEFAULT NULL,
+  `sort` varchar(5) DEFAULT NULL,
+  `category` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (7,'重乳酪蛋糕',50,2,'2018-03-21','2018-05-03','濃醇香','cheese.jpg','2018-04-06','2018-04-15','AA00001','2','蛋糕'),(8,'戚風蛋糕',70,26,'2018-04-25','2018-04-21','柔軟蓬鬆','cheese.jpg','2018-04-04','2018-04-19','AA00002','6','蛋糕'),(9,'黑森林蛋糕',40,18,'2018-04-04','2018-04-21','甜蜜滋味','cheese.jpg','2018-04-12','2018-04-28','AA00003','7','蛋糕'),(10,'紅絲絨',120,9,'2018-01-22','2018-03-01','高貴不貴','cheese.jpg','2018-01-03','2018-02-18','AA00004','4','蛋糕');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-04  4:38:00
